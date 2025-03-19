252c43fd3e593304f82a626b6dcce0d6bb10e46a
Rick Li <79388294+rickmli@users.noreply.github.com>
2025-03-18 17:37:15 -0700

    Configured post, user, comment, and tag models

---
+++ server/models/post.model.js
@@ -0,0 +1,39 @@
+import { defaultArticleImage } from "../utils/constants";
+
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: defaultArticleImage,
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
